4f6c59d2520fe460abe696bbceee6c7b32e2ee7d
Michael Mueller <michael.mueller@uos.de>
2025-02-26 13:40:39 +0100

    Started implementing redesigned CPU core allocator.

---
+++ include/core_allocator.hpp
@@ -0,0 +1,47 @@
+/*
+ * Resource Allocator
+ *
+ * Copyright (C) 2023-2025 Michael Müller <michael.mueller@uos.org>, Osnabrück University
+ *
+ * This file is part of the Tukija microhypervisor, based on NOVA.
+ *
+ * Tukija and NOVA are free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 as published by
+ * the Free Software Foundation.
+ *
+ * Tukija is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License version 2 for more details.
+ */
+
+#pragma once
+
+#include "config.hpp"
+#include "cell.hpp"
+#include "resource.hpp"
+#include "cpuset.hpp"
+#include "buddy.hpp"
+#include "stdio.hpp"
+#include "bit_alloc.hpp"
+
+class Pd;
+
+class Core_allocator
+{
+    private:
+        alignas(64) Cpu_resource *_resources{nullptr};
+
+    public:
+        Core_allocator() = default;
+
+        size_t alloc(size_t quantity, Cell *cell);
+
+        void init();
+
+        void add_cpu([[maybe_unused]] unsigned int cpu) {
+            new (&_resources[cpu]) Cpu_resource(static_cast<uint16>(cpu));
+        }
+};
+
+extern Core_allocator _core_alloc;
---
+++ src/core_allocator.cpp
@@ -0,0 +1,35 @@
+#include "core_allocator.hpp"
+#include "stdio.hpp"
+#include "pd.hpp"
+
+size_t Core_allocator::alloc(size_t quantity, Cell *cell)
+{
+    size_t cores_allocated = 0;
+    Cpuset free_affiliated_cores{0};
+
+    free_affiliated_cores.merge(cell->cip->cores_reserved);
+    free_affiliated_cores.subtract(cell->cip->cores_current);
+
+    Cpuset::for_each(free_affiliated_cores,
+        [&](long cpu)
+        {
+            if (quantity == 0)
+                return;
+            if (_resources[cpu].occupy(cell, &cell->workers_for_core(static_cast<unsigned>(cpu))))
+            {
+                cores_allocated++;
+                quantity--;
+            }
+        });
+
+    _resources[14].reclaim();
+    Console::print("Allocating %lu cores for Cell %p", quantity, cell);
+    return cores_allocated;
+}
+
+void Core_allocator::init() {
+    _resources = static_cast<Cpu_resource*>(Buddy::alloc(2, Pd::kern.quota, Buddy::FILL_0));
+    Pd::root.Space_mem::insert(Pd::kern.quota, reinterpret_cast<mword>(_resources), 2, Hpt::HPT_P | Hpt::HPT_NX | Hpt::HPT_W, Buddy::ptr_to_phys(_resources));
+}
+
+Core_allocator _core_alloc;
