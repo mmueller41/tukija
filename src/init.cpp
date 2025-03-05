da5d7c9985db1dbee4c9f0816417c8d6347069a2
Michael Mueller <michael.mueller@uos.de>
2025-03-05 14:55:53 +0100

    Changed boot message to avoid confusion with vanilla NOVA in logs.

--- src/init.cpp
+++ src/init.cpp
@@ -109,7 +109,7 @@ void init (mword magic, mword mbi)
     for (void (**func)() = &CTORS_C; func != &CTORS_G; (*func++)()) ;
 
     // Now we're ready to talk to the world
-    Console::print ("\fNOVA Microhypervisor v%d-%07lx (%s): [%s] [%s]\n", CFG_VER, reinterpret_cast<mword>(&GIT_VER), ARCH, COMPILER_STRING, magic == Multiboot::MAGIC ? "MBI" : (magic==Multiboot2::MAGIC ? "MBI2" : ""));
+    Console::print ("\fTukija Microhypervisor v%d-%07lx (%s): [%s] [%s]\n", CFG_VER, reinterpret_cast<mword>(&GIT_VER), ARCH, COMPILER_STRING, magic == Multiboot::MAGIC ? "MBI" : (magic==Multiboot2::MAGIC ? "MBI2" : ""));
     _core_alloc.init();
 
     Idt::build();
