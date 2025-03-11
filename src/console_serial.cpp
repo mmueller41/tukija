3ddbfa863464df0e0f73c94cda1bee0429c4766d
Michael Mueller <michael.mueller@uos.de>
2025-03-11 13:55:53 +0100

    Use COM2 by default.

--- src/console_serial.cpp
+++ src/console_serial.cpp
@@ -34,7 +34,7 @@ Console_serial::Console_serial()
     char *mem = static_cast<char *>(Hpt::remap (Pd::kern.quota, 0));
     if (!(base = *reinterpret_cast<uint16 *>(mem + 0x400)) &&
         !(base = *reinterpret_cast<uint16 *>(mem + 0x402)))
-        base = 0x3f8;
+        base = 0x2f8;
 
     reenable();
 
