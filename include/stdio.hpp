af769580de8c24682447b241a0e21227da1563ab
Michael Mueller <michael.mueller@uos.de>
2025-05-11 22:09:45 +0200

    Changed TRACE number for TIP debug messages.

--- include/stdio.hpp
+++ include/stdio.hpp
@@ -57,7 +57,7 @@ enum {
     TRACE_OOM       = 1UL << 24,
     TRACE_SYSCALL   = 1UL << 30,
     TRACE_ERROR     = 1UL << 31,
-    TRACE_TIP       = 1UL << 32,
+    TRACE_TIP       = 1UL << 9,
     TRACE_CELL      = 1UL << 6,
     TRACE_CORE_ALLOC= 1UL << 7,
 };
@@ -69,6 +69,7 @@ unsigned const trace_mask =
                             TRACE_ACPI      |
                             TRACE_CPU       |
                             TRACE_IOMMU     |
+                            TRACE_TIP |
 #ifdef DEBUG
                             TRACE_CELL      |
                             TRACE_DEL       |
